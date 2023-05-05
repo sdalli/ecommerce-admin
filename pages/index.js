d30f3762d920de0d1bf3fd2fde4108595f74668a
Shankar Reddy <dshankarreddy@gmail.com>
2023-05-05 15:47:32 +0530

    Gmail authentication and MongoDB

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,20 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+
 export default function Home() {
-  return (
-    <div className="bg-blue-900 w-screen h-screen flex item-center">
-      <div className="text-center w-full">
-        <button className="bg-white p-2 px-4 rounded-lg">
-          Login with Google
-        </button>
+  const { data: session } = useSession();
+  if (!session) {
+    return (
+      <div className="bg-blue-900 w-screen h-screen flex item-center">
+        <div className="text-center p-4 px-10 w-full">
+          <button
+            onClick={() => signIn()}
+            className="bg-white p-2 px-4 rounded-lg"
+          >
+            Login with Google
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+  return <div> Logged In {session.user.name} </div>;
 }
